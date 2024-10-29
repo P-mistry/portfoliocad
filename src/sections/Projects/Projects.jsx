a53f5216eff4e5344dcdfc8c6059a07c6705aee8
P-mistry <parth9773021717@gmail.com>
2024-10-29 06:07:45 -0600

    add: initial files

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,46 @@
+import styles from './ProjectsStyles.module.css';
+import viberr from '../../assets/viberr.png';
+import freshBurger from '../../assets/fresh-burger.png';
+import hipsster from '../../assets/hipsster.png';
+import fitLift from '../../assets/fitlift.png';
+import ProjectCard from '../../common/ProjectCard';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        {/* Project Card for Viberr Streaming App */}
+        <ProjectCard
+          src={viberr}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Viberr"
+          p="Streaming App"
+        />
+        {/* Project Card for Fresh Burger Restaurant */}
+        <ProjectCard
+          src={freshBurger}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Fresh Burger"
+          p="Hamburger Restaurant"
+        />
+        {/* Project Card for Hipsster Glasses Shop */}
+        <ProjectCard
+          src={hipsster}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Hipsster"
+          p="Glasses Shop"
+        />
+        {/* Project Card for FitLift Fitness App */}
+        <ProjectCard
+          src={fitLift}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="FitLift"
+          p="Fitness App"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
