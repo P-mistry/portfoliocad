f8595652ee547374a9756afc9cc9ef672d7b4467
pmistry <parth9773021717@gmail.com.com>
2025-08-09 13:43:17 -0600

    add skill animation

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,38 +1,58 @@
-import styles from './SkillsStyles.module.css';
-import checkMarkIconDark from '../../assets/checkmark-dark.svg';
-import checkMarkIconLight from '../../assets/checkmark-light.svg';
-import SkillList from '../../common/SkillList';
 import { useTheme } from '../../common/ThemeContext';
+import SkillList from '../../common/SkillList';
+import styles from './SkillsStyles.module.css';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+
+import checkMarkIconLight from '../../assets/checkmark-light.svg';
+import checkMarkIconDark from '../../assets/checkmark-dark.svg';
 
 function Skills() {
   const { theme } = useTheme();
   const checkMarkIcon = theme === 'light' ? checkMarkIconLight : checkMarkIconDark;
 
+  const skills = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "TypeScript",
+    "Node",
+    "React",
+    "Angular",
+    "Vue",
+    "Tailwind CSS",
+    "Redux",
+    "Webpack",
+    "Git",
+    "Jest",
+    "Bootstrap",
+  ];
+
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
-      <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="HTML" />
-        <SkillList src={checkMarkIcon} skill="CSS" />
-        <SkillList src={checkMarkIcon} skill="JavaScript" />
-        <SkillList src={checkMarkIcon} skill="TypeScript" />
-        <SkillList src={checkMarkIcon} skill="Node" />
-      </div>
-      <hr />
-      <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="React" />
-        <SkillList src={checkMarkIcon} skill="Angular" />
-        <SkillList src={checkMarkIcon} skill="Vue" />
-        <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
-      </div>
-      <hr />
-      <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="Redux" />
-        <SkillList src={checkMarkIcon} skill="Webpack" />
-        <SkillList src={checkMarkIcon} skill="Git" />
-        <SkillList src={checkMarkIcon} skill="Jest" />
-        <SkillList src={checkMarkIcon} skill="Bootstrap" />
-      </div>
+      <Swiper
+        modules={[Autoplay]}
+        slidesPerView="auto"
+        spaceBetween={50}
+        loop={true}
+        speed={7000}
+        autoplay={{
+          delay: 0,
+          disableOnInteraction: false,
+          reverseDirection: false,
+        }}
+        allowTouchMove={false}
+        className={styles.skillSwiper}
+      >
+        {skills.concat(skills).map((skill, i) => (
+          <SwiperSlide key={i} className={styles.skillSlide}>
+            <SkillList src={checkMarkIcon} skill={skill} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
   );
 }
