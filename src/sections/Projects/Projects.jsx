0dc847fcd4fd40a1a42e4b82d31374215a5c2c35
pmistry <parth9773021717@gmail.com.com>
2025-08-10 12:38:47 -0600

    Fix text align

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -36,7 +36,7 @@ function Projects() {
 
   return (
     <section id="projects" className={styles.container}>
-      <h1 className="sectionTitle">Projects</h1>
+            <h1 className="sectionTitle">Projects</h1>
       <ul className={styles.list}>
         {projectEntries.map((project) => (
           <li key={project.title} className={styles.item}>
